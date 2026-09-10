98480ae586ce28be19ac002b927ea77d0b11c2b6
Taleb88 <tdamaree18@gmail.com>
2026-09-10 18:35:46 -0400

    various updates

--- analysis.py
+++ analysis.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv('csvs_created/All Beauty.csv') # 4266963 rows
+df = pd.read_csv('csvs_created/All Beauty 1.csv')
 
 df_1 = df[:500000]
 
 # print(df_1)
 
-df_1.to_csv('test.csv')
+# df_1.to_csv('test.csv')
