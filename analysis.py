7669537839f9e902a0c025bc3335153ce70eb706
Taleb88 <tdamaree18@gmail.com>
2026-09-10 23:56:45 -0400

    various updates

--- analysis.py
+++ analysis.py
@@ -4,5 +4,7 @@ import pandas as pd
 # df = pd.read_csv('raw_file/amazon_reviews_actual_5_5m.csv')
 reviews_df = pd.read_csv('csvs_created/Computers.csv')
 
-reviews_df_pivot_table = reviews_df.pivot_table(index='product_id', columns='review_datetime_utc', values='review_score', aggfunc='mean')
-print(reviews_df_pivot_table)
+reviews_df_pivot_table = reviews_df.pivot_table(index='product_id', columns='review_score', values='product_rating_count', aggfunc='mean')
+print(reviews_df_pivot_table.head(30))
+
+# reviews_df_pivot_table.to_csv('test.csv')
