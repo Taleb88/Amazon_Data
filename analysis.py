10bcb81f528242c1e49ebee33024a2472d5bb105
Taleb88 <tdamaree18@gmail.com>
2026-09-10 23:28:35 -0400

    various updates

--- analysis.py
+++ analysis.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv('csvs_created/All Beauty 1.csv')
+# df = pd.read_csv('csvs_created/All Beauty 1.csv')
+df = pd.read_csv('raw_file/amazon_reviews_actual_5_5m.csv')
 
-print(df.head(20))
+# print(df.head(20))
 
-test = df.head(30)
-test.to_csv('test.csv', index=False)
+print(df[df['product_id'] == 'B010TWN80W'])
