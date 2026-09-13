35d01fc45a9648725999c3d8a48d6933e99c2faf
Taleb88 <tdamaree18@gmail.com>
2026-09-13 14:59:07 -0400

    code refactored

--- analysis.py
+++ analysis.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-# df = pd.read_csv('csvs_created/All Beauty 1.csv')
+df = pd.read_csv('csvs_created/All Beauty 1.csv')
 # df = pd.read_csv('raw_file/amazon_reviews_actual_5_5m.csv')
-all_beauty_pivot_table = pd.read_csv('pivot_tables/All Beauty_reviews_pivot_table.csv')
+# all_beauty_pivot_table = pd.read_csv('pivot_tables/All Beauty_reviews_pivot_table.csv')
 
-print(all_beauty_pivot_table[all_beauty_pivot_table['1.0'].notna()])
+print(df.head(20))
+
+df = df.head(20)
+df.to_csv('test.csv')
